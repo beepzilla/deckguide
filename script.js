6d930930c1d19d953d650ad5295e4d9a1a79b79c
Josh <beepzilla@gmail.com>
2024-03-26 03:02:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ option = {
   backgroundColor: "#171717",
   color: ["#ef476f", "#f4a261", "#ffd166", "#06d6a0", "#118ab2", "#073b4c", "#f8961e"],
   legend: {
-    show: true
+    show: true,
+    textStyle: {
+      color: "#ffffff",
+      fontFamily: "Press Start 2P",
+      fontSize: 12,
+    },
   },
   toolbox: {
     show: false,
@@ -22,8 +27,11 @@ option = {
       label: {
         position: "outer",
         formatter: "{b}: {d}%",
-        fontFamily: 'Press Start 2P',
+        fontFamily: "Press Start 2P",
         fontSize: 12,
+        color: "#ffffff",
+        overflow: "break",
+        lineHeight: 18,
       },
       data: [
         { value: 35, name: "CoinSale" },
